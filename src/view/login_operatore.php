4d22e0f8036021e30b3b0a4590c8e32d0439b965
GaiaMazzoni <gaia.mazzoni2@studio.unibo.it>
2024-04-02 15:36:20 +0200

    aggiunti login differenziati in base a funzione

---
+++ src/view/login_operatore.php
@@ -0,0 +1,48 @@
+<!--<?php
+session_start();
+include_once("../includes/connection.php");
+if(isset($_SESSION['login_error'])){
+    echo '<div class="alert alert-danger">' . $_SESSION['login_error'] . '</div>';
+    unset($_SESSION['login_error']);
+}
+?>
+-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Log In Operatore</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!--<link rel="stylesheet" type="text/css" href="../css/login.css">-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+    <div class="row">
+        <div class="col-12 text-center">
+            <img src="../../images/icona.png" alt="FoodBook_icon" class="img-rounded" title="FoodBook_icon" width="100" 
+                height="100" id = "icon">
+        </div>
+    </div>
+    <div class="main-content">
+        <form action="../api/login_logics.php" method="post" class="row">
+            <div class="col-sm-12">
+                <div class="input-group">
+                    <span class="input-group-addon"><em class="glyphicon glyphicon-pencil"></em></span>
+                    <label for="badge_number"></label>
+                    <input type="text" id="badge_number" class="form-control" placeholder="Numero Badge" name="badge_number" required>
+                </div>
+                <div class="input-group">
+                    <span class="input-group-addon"><em class="glyphicon glyphicon-pencil"></em></span>
+                    <label for="password"></label>
+                    <input type="password" id="password" class="form-control" placeholder="Password" name="password" required>
+                </div>
+                <p id="back_to_index"><a href="index.php">Non sei un operatore?</a></p>
+            </div>
+            <div class="text-center">
+                <button type="submit" class="btn btn-info w-50" name="login_to_signup" id="login">Log In</button>
+            </div>
+        </form>
+    </div>
+</body>
+</html>
