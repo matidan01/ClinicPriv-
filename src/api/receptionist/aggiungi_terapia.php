8c71d65c6ab5def563d943d08b04c10a1512981e
matidan01 <matildedantino@gmail.com>
2024-04-17 10:48:26 +0200

    Aggiunta nuova terapia

---
+++ src/api/receptionist/aggiungi_terapia.php
@@ -0,0 +1,45 @@
+<?php
+include_once("../../includes/connection.php");
+include_once("../../includes/database.php");
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $idPaziente = $_POST['id'];
+    $dataScadenza = $_POST['dataScadenza'];
+    $idMedico = $_POST['idMedico'];
+    if($idMedico == '') {
+        $idMedico = NULL;
+    }
+
+    $farmaci = array();
+    $descrizioni = array();
+
+    foreach ($_POST['nomeFarmaco'] as $key => $value) {
+        $farmaci[] = $value;
+        $descrizioni[] = $_POST['descrizione'][$key];
+    }
+
+    if(count($farmaci) > 0) {
+        $idTerapia = get_last_id_terapia($con) + 1;
+        $insert_terapia_query = "INSERT INTO `terapia`(`idTerapia`,`idPaziente`, `dataScadenza`, `idMedico`) VALUES (?, ?, ?, ?)";
+        $insert_terapia_stmt = mysqli_prepare($con, $insert_terapia_query);
+        mysqli_stmt_bind_param($insert_terapia_stmt, 'iiss', $idTerapia, $idPaziente, $dataScadenza, $idMedico);
+        mysqli_stmt_execute($insert_terapia_stmt);
+
+        if(mysqli_affected_rows($con) > 0) {
+
+            $insert_prescrizione_query = "INSERT INTO `prescrizione`(`idTerapia`, `nomeFarmaco`, `descrizione`) VALUES (?, ?, ?)";
+            $insert_prescrizione_stmt = mysqli_prepare($con, $insert_prescrizione_query);
+            mysqli_stmt_bind_param($insert_prescrizione_stmt, 'iss', $idTerapia, $nomeFarmaco, $descrizione);
+
+            for($i = 0; $i < count($farmaci); $i++) {
+                $nomeFarmaco = $farmaci[$i];
+                $descrizione = $descrizioni[$i];
+                mysqli_stmt_execute($insert_prescrizione_stmt);
+            }
+        }
+    }
+
+    header("Location: ../../view/receptionist/profilo_paziente.php?idPaziente=" . urlencode($idPaziente));
+}
+
+
--- src/includes/database.php
+++ src/includes/database.php
@@ -7,6 +7,12 @@ function get_last_id_paziente($mysqli) {
     return $stmt->get_result()->fetch_assoc()["max(idPaziente)"];
 }
 
+function get_last_id_terapia($mysqli) {
+    $stmt = $mysqli->prepare("SELECT max(idTerapia) FROM terapia");
+    $stmt->execute();
+    return $stmt->get_result()->fetch_assoc()["max(idTerapia)"];
+}
+
 function elimina_dati_paziente($mysqli, $idPaziente) {
     $stmt = $mysqli->prepare("DELETE FROM paziente WHERE idPaziente = ?");
     $stmt->bind_param("i",$idPaziente);
